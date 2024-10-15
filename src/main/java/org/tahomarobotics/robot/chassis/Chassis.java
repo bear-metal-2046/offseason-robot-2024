7ff10072bfe6c42b08a5c5f8eda87be1c245f5bc
Nicolas <nicsilvc@gmail.com>
2024-10-15 16:15:12 -0700

    set up chassis

--- src/main/java/org/tahomarobotics/robot/chassis/Chassis.java
+++ src/main/java/org/tahomarobotics/robot/chassis/Chassis.java
@@ -1,13 +1,21 @@
 package org.tahomarobotics.robot.chassis;
 
-import com.ctre.phoenix6.hardware.Pigeon2;
+import com.ctre.phoenix6.BaseStatusSignal;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.struct.SwerveModuleStateStruct;
+import edu.wpi.first.wpilibj.Threads;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import org.tahomarobotics.robot.RobotConfiguration;
 import org.tahomarobotics.robot.RobotMap;
 import org.tahomarobotics.robot.util.CalibrationData;
 
-import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Chassis {
@@ -18,10 +26,12 @@ public class Chassis {
 
     private ChassisSpeeds targetSpeeds = new ChassisSpeeds();
     private final CalibrationData<Double[]> swerveCalibration;
+    private final SwerveDriveKinematics kinematics;
     private final SwerveDrivePoseEstimator poseEstimator;
     private final Field2d fieldPose = new Field2d();
     private final Thread odometryThread;
     private boolean isFieldCentric = true;
+    private SwerveModulePosition[] lastModulePosition;
 
     private Chassis() {
         // Read calibration from rio
@@ -35,5 +45,54 @@ public class Chassis {
                 new SwerveModule(RobotMap.BACK_LEFT_MOD, angularOffsets[2]),
                 new SwerveModule(RobotMap.BACK_RIGHT_MOD, angularOffsets[3])
         );
+
+        kinematics = new SwerveDriveKinematics(
+                modules.stream()
+                        .map(SwerveModule::getTranslationOffset)
+                        .toArray(Translation2d[]::new)
+        );
+
+        lastModulePosition = getSwerveModulePositions();
+
+        poseEstimator = new SwerveDrivePoseEstimator(
+                kinematics,
+                new Rotation2d(),
+                getSwerveModulePositions(),
+                new Pose2d(0.0, 0.0, new Rotation2d(0.0))
+        );
+
+        odometryThread = new Thread(this::odometryThread);
+        odometryThread.start();
+    }
+
+    public static Chassis getInstance() {
+        return INSTANCE;
+    }
+
+    public SwerveModulePosition[] getSwerveModulePositions() {
+        return modules.stream().map(SwerveModule::getPosition).toArray(SwerveModulePosition[]::new);
+    }
+
+    private void odometryThread() {
+        Threads.setCurrentThreadPriority(true, 1);
+
+        // Get signals array
+        List<BaseStatusSignal> signalList = new ArrayList<>(pigeon.getStatusSignals());
+        for (var module : this.modules) {
+            signalList.addAll(module.getStatusSignals());
+        }
+
+        BaseStatusSignal[] signals = signalList.toArray(BaseStatusSignal[]::new);
+
+
+        while (true) {
+            // Wait for all signals to arrive
+            BaseStatusSignal.waitForAll(4 / RobotConfiguration.ODOMETRY_UPDATE_FREQUENCY, signals);
+            updatePosition();
+        }
+    }
+
+    private void updatePosition() {
+
     }
 }
--- src/main/java/org/tahomarobotics/robot/chassis/SwerveModule.java
+++ src/main/java/org/tahomarobotics/robot/chassis/SwerveModule.java
@@ -1,9 +1,15 @@
 package org.tahomarobotics.robot.chassis;
 
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.tahomarobotics.robot.RobotMap;
+
+import java.util.List;
 
 public class SwerveModule {
     private static final Logger logger = LoggerFactory.getLogger(SwerveModule.class);
@@ -11,10 +17,46 @@ public class SwerveModule {
     public final String name;
     private final Translation2d translationOffset;
     private double angularOffset;
-    public SwerveModule(RobotMap.SwerveModuleDescriptor descriptor, double angularOffset){
 
+    private final StatusSignal<Double> steerPosition;
+    private final StatusSignal<Double> steerVelocity;
+    private final StatusSignal<Double> drivePosition;
+    private final StatusSignal<Double> driveVelocity;
+    private final StatusSignal<Double> driveAcceleration;
+    private final StatusSignal<Double> driveCurrent;
+    private final StatusSignal<Double> steerCurrent;
+
+    public SwerveModule(RobotMap.SwerveModuleDescriptor descriptor, double angularOffset){
         name = descriptor.moduleName();
         translationOffset = descriptor.offset();
         this.angularOffset = angularOffset;
+    }
+
+    public Translation2d getTranslationOffset() {
+        return translationOffset;
+    }
+
+    public SwerveModulePosition getPosition() {
+        return new SwerveModulePosition(getDrivePosition(), Rotation2d.fromRotations(getSteerAngle()));
+    }
+
+    private double getSteerAngle() {
+        return 0;
+    }
+
+    private double getDrivePosition() {
+        return 0;
+    }
+
+    public List<BaseStatusSignal> getStatusSignals() {
+        return List.of(
+                drivePosition,
+                driveAcceleration,
+                driveVelocity,
+                steerPosition,
+                steerVelocity,
+                driveCurrent,
+                steerCurrent
+        );
     }
 }
