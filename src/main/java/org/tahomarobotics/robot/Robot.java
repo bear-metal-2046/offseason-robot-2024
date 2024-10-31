f3459b6df30a74ef2d16b80962e39109b50ebf4b
Nicolas <nicsilvc@gmail.com>
2024-10-31 16:34:10 -0700

    Teleop drive command & pid

--- src/main/java/org/tahomarobotics/robot/Robot.java
+++ src/main/java/org/tahomarobotics/robot/Robot.java
@@ -8,6 +8,11 @@ package org.tahomarobotics.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import org.tahomarobotics.robot.chassis.Chassis;
+import org.tahomarobotics.robot.util.SubsystemIF;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The VM is configured to automatically run this class, and to call the methods corresponding to
@@ -15,11 +20,10 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
-public class Robot extends TimedRobot
-{
+public class Robot extends TimedRobot {
+    private final List<SubsystemIF> subsystems = new ArrayList<>();
+
     private Command autonomousCommand;
-    
-    private OI robotContainer;
     
     
     /**
@@ -27,11 +31,9 @@ public class Robot extends TimedRobot
      * initialization code.
      */
     @Override
-    public void robotInit()
-    {
-        // Instantiate our OI.  This will perform all our button bindings, and put our
-        // autonomous chooser on the dashboard.
-        robotContainer = new OI();
+    public void robotInit() {
+        subsystems.add(OI.getInstance());
+        subsystems.add(Chassis.getInstance());
     }
     
     
@@ -43,8 +45,7 @@ public class Robot extends TimedRobot
      * SmartDashboard integrated updating.
      */
     @Override
-    public void robotPeriodic()
-    {
+    public void robotPeriodic() {
         // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
         // commands, running already-scheduled commands, removing finished or interrupted commands,
         // and running subsystem periodic() methods.  This must be called from the robot's periodic
@@ -84,14 +85,7 @@ public class Robot extends TimedRobot
     @Override
     public void teleopInit()
     {
-        // This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to
-        // continue until interrupted by another command, remove
-        // this line or comment it out.
-        if (autonomousCommand != null)
-        {
-            autonomousCommand.cancel();
-        }
+        subsystems.forEach(SubsystemIF::onTeleopInit);
     }
     
     
--- src/main/java/org/tahomarobotics/robot/chassis/ChassisConstants.java
+++ src/main/java/org/tahomarobotics/robot/chassis/ChassisConstants.java
@@ -1,16 +1,11 @@
 package org.tahomarobotics.robot.chassis;
 
-import com.ctre.phoenix6.configs.AudioConfigs;
-import com.ctre.phoenix6.configs.MagnetSensorConfigs;
-import com.ctre.phoenix6.configs.MotorOutputConfigs;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.signals.AbsoluteSensorRangeValue;
-import com.ctre.phoenix6.signals.InvertedValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
-import com.ctre.phoenix6.signals.SensorDirectionValue;
+import com.ctre.phoenix6.configs.*;
+import com.ctre.phoenix6.signals.*;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.system.plant.DCMotor;
+import org.tahomarobotics.robot.RobotConfiguration;
 
 public class ChassisConstants {
 
@@ -21,7 +16,8 @@ public class ChassisConstants {
 
     public static final double WHEEL_RADIUS = 0.0508;
     public static final double WHEEL_CIRCUMFERENCE = 2 * Math.PI * WHEEL_RADIUS;
-    public static final double DRIVE_REDUCTION = 1;
+    public static final double DRIVE_REDUCTION = (14.0 / 50.0) * (27.0 / 17.0) * (15.0 / 45.0);
+    public static final double STEER_REDUCTION = (14.0 / 50.0) * (10.0 / 60.0);
     public static final double DRIVE_POSITION_COEFFICIENT = WHEEL_CIRCUMFERENCE * DRIVE_REDUCTION;
 
     public static final DCMotor SWERVE_MOTOR = DCMotor.getKrakenX60(1);
@@ -34,18 +30,48 @@ public class ChassisConstants {
     public static final Translation2d BACK_LEFT_OFFSET = new Translation2d(-HALF_WHEELBASE, HALF_TRACK_WIDTH);
     public static final Translation2d BACK_RIGHT_OFFSET = new Translation2d(-HALF_WHEELBASE, -HALF_TRACK_WIDTH);
 
+    public static final double kV_DRIVE = (2 * Math.PI) / SWERVE_MOTOR.KvRadPerSecPerVolt;
+
     public static final TalonFXConfiguration driveMotorConfiguration = new TalonFXConfiguration()
+            .withSlot0(new Slot0Configs()
+                    .withKP(0.15)
+                    .withKV(kV_DRIVE))
             .withMotorOutput(new MotorOutputConfigs()
                     .withNeutralMode(NeutralModeValue.Brake)
                     .withInverted(InvertedValue.Clockwise_Positive))
+            .withMotionMagic(new MotionMagicConfigs()
+                    .withMotionMagicAcceleration(120)
+                    .withMotionMagicJerk(360)
+            )
             .withAudio(new AudioConfigs()
                     .withBeepOnBoot(true)
                     .withBeepOnConfig(true));
 
     public static final TalonFXConfiguration steerMotorConfiguration = new TalonFXConfiguration()
+            .withSlot0(new Slot0Configs()
+                    .withKP(8.0)
+                    .withKI(0.01)
+                    .withKD(0.16)
+            )
             .withMotorOutput(new MotorOutputConfigs()
                     .withNeutralMode(NeutralModeValue.Brake)
                     .withInverted(InvertedValue.Clockwise_Positive))
+            .withMotionMagic(new MotionMagicConfigs()
+                    .withMotionMagicAcceleration(25)
+                    .withMotionMagicJerk(100)
+            )
+            .withClosedLoopGeneral(new ClosedLoopGeneralConfigs() {{
+                ContinuousWrap = true;
+            }})
+            .withFeedback(new FeedbackConfigs() {{
+                              if (RobotConfiguration.CANIVORE_PHOENIX_PRO) {
+                                  FeedbackSensorSource = FeedbackSensorSourceValue.FusedCANcoder;
+                                  RotorToSensorRatio = 1 / STEER_REDUCTION;
+                              } else {
+                                  FeedbackSensorSource = FeedbackSensorSourceValue.RemoteCANcoder;
+                              }
+                          }}
+            )
             .withAudio(new AudioConfigs()
                     .withBeepOnBoot(true)
                     .withBeepOnConfig(true));
